0ef5c44e861d4139cb652a04c8e160ab817e9839
sotnikovuu <sotnikovuu@yandex.ru>
2022-05-29 20:59:50 +0300

    lesson_3.prepared

---
+++ frontend/src/components/user.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.firstname}
+            </td>
+            <td>
+                {user.lastname}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UsersList = ({users}) => {
+    return (
+        <table>
+            <th>
+                username
+            </th>
+            <th>
+                firstname
+            </th>
+            <th>
+                lastname
+            </th>
+            <th>
+                email
+            </th>
+                {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+    export default UsersList
